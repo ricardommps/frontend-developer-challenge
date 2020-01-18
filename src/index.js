f38c79d087d504be567b98c16b9a9999f2495bbd
supero.ricardo <ricardo.silva@supero.com.br>
2020-01-17 21:00:35 -0300

    start project

--- src/index.js
+++ src/index.js
@@ -1,37 +1,12 @@
-// @flow
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
-import { Provider } from 'react-redux'
-import createSagaMiddleware from 'redux-saga'
-import rootReducer from './reducers'
-import rootSaga from './sagas'
-import App from './App'
-import 'normalize.css/normalize.css'
-import 'font-awesome/css/font-awesome.min.css'
-import './styles/main.css'
-import type { Store } from './types'
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const sagaMiddleware = createSagaMiddleware()
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware))
-const store: Store = createStore(rootReducer, enhancer)
-
-const element = document.getElementById('root')
-if (!element) {
-  throw new Error('couldn\'t find element with id root')
-}
-
-sagaMiddleware.run(rootSaga)
-
-render(
-  <Provider store={store}>
-    <div>
-      <App />
-      {/*<DevTools />*/}
-    </div>
-  </Provider>,
-  element
-)
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- src/services/products.js
+++
@@ -1,6 +0,0 @@
-import type { Product, Products } from '../types/products'
-import service from '../api/Api'
-
-export function fetchProductsFromApi(page: number): Promise < Product > | Promise < Products > {
-    return service.get(`/products?page=${page}`)
-}
