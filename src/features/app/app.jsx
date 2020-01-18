caba8ee9e632968819f50875c780082ee020346e
supero.ricardo <ricardo.silva@supero.com.br>
2020-01-18 13:24:34 -0300

    criado componente footer

--- src/features/app/app.jsx
+++ src/features/app/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../header/Header';
+import Footer from '../footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/features/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+import './Footer.scss';
+
+const Footer = () => (
+  <div className="footer">
+    <div className="row text-container">
+      <Typography>Testando suas habilidades em HTML, CSS e JS.</Typography>
+      <Typography>Linx Impulse</Typography>
+      <Typography>2019</Typography>
+    </div>
+  </div>
+);
+
+export default Footer;
